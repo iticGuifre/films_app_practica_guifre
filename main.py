6abbc5389f8b47db08f529df8a97f65c7e926fb8
iticGuifre <2023_guifre.salvat@iticbcn.cat>
2024-02-20 19:08:32 +0100

    Metodes crud fets

--- main.py
+++ main.py
@@ -9,20 +9,30 @@ mycursor = mydb.cursor()
 mycursor.execute("USE pelis")
 
 def cerca_per_camp(camp: str, valor):
-    # if type(valor) == 'int':
-    #     valor = int(valor)
+    if type(valor) == str:
+        valor = f"'{valor}'"
     mycursor.execute(f"SELECT * FROM PELICULA WHERE {camp} = {valor}")
     print(mydb)
     show_lines(mycursor)
 
 def afegir_pelicula(titol, any, puntuacio, vots):
-    mycursor.execute(f"INSERT PELICULA * (TITULO, ANYO, PUNTUACION, VOTOS) VALUES ({titol}, {any}, {puntuacio}, {vots})")
+    mycursor.execute(f"INSERT PELICULA (TITULO, ANYO, PUNTUACION, VOTOS) VALUES ('{titol}', {any}, {puntuacio}, {vots})")
     print(mydb)
     show_lines(mycursor)
+    mydb.commit()
+
+def modificar_pelicula(id: int, titol, any, puntuacio, vots):
+    mycursor.execute(f"UPDATE PELICULA SET TITULO = '{titol}', ANYO = {any}, PUNTUACION = {puntuacio}, VOTOS = {vots} WHERE ID = {id};")
+    print(mydb)
+    show_lines(mycursor)
+    mydb.commit()
 
 #METODE PER MOSTRAR CONSULTA
 def show_lines(cursor):
     for x in cursor:
         print(x)
 
-cerca_per_camp("ID", "0")
+# afegir_pelicula("Salvat", 2024, 10, 5)
+# cerca_per_camp("TITULO", "Salvat")
+# modificar_pelicula(1705, "Poma", 2025, 10 , 5)
+# cerca_per_camp("ID", 1705)
